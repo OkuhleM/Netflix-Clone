c57076de4da47d36a8b00f7eacb9e8402f7ea17a
OkuhleM <lindokuhlea38@gmail.com>
2025-10-29 23:51:20 +0200

    add loading page

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,25 +1,34 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
-import '../Styling/Navbar.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import "../Styling/Navbar.css";
 
 function Navbar() {
-
   return (
     <div>
-        <nav className='navbar'>
-<h1 className='zayn'>ZaynFlix 💖</h1>
-<div className="nav-links">
-     <ul className="nav">
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/albums">Albums</Link></li>
-        <li><Link to="/singles">Singles</Link></li>
-        <li><Link to="/music-videos">Music Videos</Link></li>
-        <li><input type='search' /></li>
-      </ul>
+      <nav className="navbar">
+        <h1 className="zayn">ZaynFlix 💖</h1>
+        <div className="nav-links">
+          <ul className="nav">
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/albums">Albums</Link>
+            </li>
+            <li>
+              <Link to="/singles">Singles</Link>
+            </li>
+            <li>
+              <Link to="/music-videos">Music Videos</Link>
+            </li>
+            <li>
+              <input type="search" />
+            </li>
+          </ul>
+        </div>
+      </nav>
     </div>
-        </nav>
-    </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
