bc3a24338036fd5c2ad6fd4211c8e256e21b4a92
OkuhleM <lindokuhlea38@gmail.com>
2025-10-31 17:18:39 +0200

    add albums api data

---
+++ src/Data/zaynData.js
@@ -0,0 +1,22 @@
+export const albums = [
+  {
+    title: "Mind of Mine",
+    image: "../Assets/Mindo_of_mine.jpg",
+    link: "https://www.youtube.com/watch?v=go8_ZWFdYGs&list=OLAK5uy_nrbHEhhkZIpj3-XdSQm75NdaqxRScGpQc",
+  },
+  {
+    title: "Icarus Falls",
+    image: "../Assets/Icarus_falls.jpg",
+    link: "https://www.youtube.com/watch?v=J-dv_DcDD_A&list=OLAK5uy_kfGBYHa-Wm6DHcn8p-n9NIf8xIRrNgYjw",
+  },
+  {
+    title: "Nobody is Listening",
+    image: "../Assets/nobody_is_listening.jpg",
+    link: "https://www.youtube.com/watch?v=_QzFnZ5oyTw&list=OLAK5uy_m5BRSFjdQ4xuwKUuNeUI6BQ0wwWFBI01c"
+  },
+  {
+    title: "Room Under the Stairs",
+    image: "../Assets/Room_under_the_stairs.jpg",
+    link: "https://www.youtube.com/watch?v=go5IoTKxevo&list=OLAK5uy_m2WsN-tgpnNcLysgfIOaOZZcWtxm-Bnlk"
+  }
+];
