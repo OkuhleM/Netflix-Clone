47d7c96b828e7f78f31820fde17fa77eef4f0c3f
OkuhleM <lindokuhlea38@gmail.com>
2025-11-06 00:20:31 +0200

    add a responsive navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,31 +1,51 @@
-import React from "react";
+import React, {useState} from "react";
 import { Link } from "react-router-dom";
 import "../Styling/Navbar.css";
+import { Menu, X } from "lucide-react";
 
 function Navbar() {
+
+   const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const closeMenu = () => {
+    setIsOpen(false);
+  };
+
   return (
     <div>
       <nav className="navbar">
+        <div className="navbar-container">
         <h1 className="zayn">ZaynFlix 💖</h1>
-        <div className="nav-links">
-          <ul className="nav">
+
+        {/* Hamburger Icon for Mobile */}
+        <div className="menu-icon" onClick={toggleMenu}>
+          {isOpen ? <X size={28} /> : <Menu size={28} />}
+        </div> 
+
+        
+          <ul className={`nav-links ${isOpen ? "active" : ""}`}>
             <li>
-              <Link to="/">Home</Link>
+              <Link to="/" onClick={closeMenu}>Home</Link>
             </li>
             <li>
-              <Link to="/about">About</Link>
+              <Link to="/about" onClick={closeMenu}>About</Link>
             </li>
             <li>
-              <Link to="/albums">Albums</Link>
+              <Link to="/albums" onClick={closeMenu}>Albums</Link>
             </li>
             <li>
-              <Link to="/singles">Singles</Link>
+              <Link to="/singles" onClick={closeMenu}>Singles</Link>
             </li>
             <li>
-              <Link to="/videos">Music Videos</Link>
+              <Link to="/videos" onClick={closeMenu}> Music Videos</Link>
             </li>
             
           </ul>
+        
         </div>
       </nav>
     </div>
