31d186442f368636eafabb01c6289da047b1ecd6
OkuhleM <lindokuhlea38@gmail.com>
2025-11-05 21:48:04 +0200

    add thumbnails

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -1,40 +1,54 @@
 import React from "react";
 import "../Styling/Banner.css";
 import zaynBanner from "../Assets/ZaynBanner.jpg";
+import {albums} from "../Data/zaynData"
 
 function Banner() {
 
     
   return (
     <div className="banner">
-      <img src={zaynBanner} alt="Zayn Malik" className="banner-img" />
-      <div className="banner-overlay"></div>
+      <div className="featured-card">
 
-      <div className="banner-content">
-        <h1>ZAYNFLIX 💖</h1>
-        <p>
-          Dive into the world of Zayn Malik — where every beat tells a story, 
-          and every lyric burns slow.
-        </p>
-        <div className="banner-buttons">
-          <a
-            href="https://www.youtube.com/@Zayn"
-            target="_blank"
-            rel="noreferrer"
-            className="banner-btn"
-          >
-            Watch on YouTube
-          </a>
-          <a
-            href="https://open.spotify.com/artist/5ZsFI1h6hIdQRw2ti0hz81"
-            target="_blank"
-            rel="noreferrer"
-            className="banner-btn secondary"
-          >
-            Listen on Spotify
-          </a>
-        </div>
+      <img src={zaynBanner} alt="Zayn Malik" className="banner-img" />
       </div>
+      <div className="featured-info">
+        <h1 className="album-title"> Room Under The Stairs</h1>
+        <p className="album-details"> 2024 * Includes " Alienate" * "Stardust" </p>
+        <button className="listen-btn">LISTEN NOW</button>
+      </div>
+
+<div className="albums-section">
+  <h2>Albums</h2>
+
+  <div className="albums-grid">
+    {albums.map((album)=>(
+      <div key={album.id} className="album-card">
+              <img
+                src={album.image}
+                alt={album.title}
+                className="album-cover"
+              />
+              <div className="album-info">
+                <h2>{album.title}</h2>
+                <p>{album.description}</p>
+                <span className="album-year">{album.year}</span>
+
+                <a
+                  href={album.link}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="album-btn"
+                >
+                 Listen on YouTube
+                </a>
+              </div>
+            </div>
+    ))
+    }
+  </div>
+</div>
+
     </div>
   );
 }
