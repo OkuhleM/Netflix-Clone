40c3cf21303ba512abb85c156d2f1d9403405179
OkuhleM <lindokuhlea38@gmail.com>
2026-04-16 22:09:51 +0200

    add a new era

--- src/Components/NewEra.js
+++ src/Components/NewEra.js
@@ -1,8 +1,31 @@
 import React from 'react'
-
+import { latestReleases } from '../Data/latestRelease'
 function NewEra() {
   return (
-    <div>NewEra</div>
+    <div className='new-era'>
+        <div className="upcoming-album">
+
+
+    <img
+      src={latestReleases.upcomingAlbum.image}
+      alt="Upcoming Album"
+    />
+
+    <div className="album-overlay">
+      <h1>{latestReleases.upcomingAlbum.title}</h1>
+      <p>{latestReleases.upcomingAlbum.description}</p>
+
+      <a
+        href={latestReleases.upcomingAlbum.link}
+        target="_blank"
+        rel="noreferrer"
+        className="listen-btn"
+      >
+        🔥 Pre-Listen
+      </a>
+    </div>
+    </div>
+    </div>
   )
 }
 
---
+++ src/Data/latestRelease.js
@@ -0,0 +1,30 @@
+import Sideways from '../Assets/SideWays.jpg'
+import DieForMe from '../Assets/Die-For-Me.jpg'
+import Konnakol from '../Assets/Konnakol-album.jpg'
+
+
+export const latestReleases = {
+  upcomingAlbum: {
+    title: "Konnakol",
+    year: 2024,
+    description: "A new chapter. Raw, stripped, and deeply sexy.",
+    image: Konnakol,
+    link: "https://www.youtube.com/results?search_query=zayn+room+under+the+stairs"
+  },
+
+  recentSingles: [
+    {
+      id: 1,
+      title: "Die For Me",
+      image: DieForMe,
+      link: "https://www.youtube.com/watch?v=your-link"
+    },
+    {
+      id: 2,
+      title: "Side Ways",
+      image: Sideways,
+      link: "https://www.youtube.com/watch?v=RQ4noGB54a8",
+
+    }
+  ]
+};
